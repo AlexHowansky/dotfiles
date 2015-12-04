306d25de0ead2627873cd74e63d769dcad9c0b83
Alex Howansky <ahowansky@coresense.com>
2015-12-04 13:58:17 -0500

    Added powerline-shell invocation.

--- .my.bash
+++ .my.bash
@@ -1,50 +1,5 @@
-function color()
-{
-    case ${1} in
-        'black') CODE='0;30' ;;
-        'red') CODE='0;31' ;;
-        'green') CODE='0;32' ;;
-        'yellow') CODE='0;33' ;;
-        'blue') CODE='0;34' ;;
-        'purple') CODE='0;35' ;;
-        'cyan') CODE='0;36' ;;
-        'light_gray') CODE='0;37' ;;
-        'dark_gray') CODE='1;30' ;;
-        'bold_red') CODE='1;31' ;;
-        'bold_green') CODE='1;32' ;;
-        'bold_yellow') CODE='1;33' ;;
-        'bold_blue') CODE='1;34' ;;
-        'bold_purple') CODE='1;35' ;;
-        'bold_cyan') CODE='1;36' ;;
-        'white') CODE='1;37' ;;
-        *) return ;;
-    esac
-    echo -ne "\[\033[${CODE}m\]"
-}
-
-function cd()
-{
-    builtin cd "${@}"
-
-    C_PUNC="$(color light_gray)"
-    C_USER="$(color bold_green)"
-    C_HOST="$(color bold_green)"
-    C_DIR="$(color bold_yellow)"
-    C_GITB="$(color cyan)"
-    C_GITM="$(color red)"
-
-    BRANCH=$(/usr/bin/env git branch 2>/dev/null | grep '^*' | cut -c3-)
-    if [ "${BRANCH}" == "" ]
-    then
-        export PS1="${C_PUNC}[${C_USER}\u${C_PUNC}@${C_HOST}\H${C_PUNC}] ${C_DIR}\w${C_PUNC}: "
-    else
-        if [ "${BRANCH}" == "master" ]
-        then
-            export PS1="${C_PUNC}[${C_USER}\u${C_PUNC}@${C_HOST}\H${C_PUNC}] ${C_DIR}\w ${C_PUNC}[${C_GITM}${BRANCH}${C_PUNC}]: "
-        else
-            export PS1="${C_PUNC}[${C_USER}\u${C_PUNC}@${C_HOST}\H${C_PUNC}] ${C_DIR}\w ${C_PUNC}[${C_GITB}${BRANCH}${C_PUNC}]: "
-        fi
-    fi
+function _update_ps1() {
+    export PS1="$(~/bin/powerline-shell.py --cwd-mode plain --mode patched --colorize-hostname $? 2> /dev/null)"
 }
 
 function git()
@@ -102,6 +57,7 @@ umask 077
 set -o vi
 
 [ "$TERM" != "dumb" ] && alias ls='ls --color=auto'
+
 alias diff='colordiff'
 alias cls='clear'
 alias grpe='grep'
@@ -112,5 +68,4 @@ alias ..='cd ..'
 [ -f ~/.git-flow-completion.bash ] && . ~/.git-flow-completion.bash
 
 export EDITOR=/usr/bin/vim
-
-cd .
+export PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
