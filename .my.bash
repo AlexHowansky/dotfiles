7ba3c98182f2b841abf6cfc29e2b8b1afa76b508
Alex Howansky <alex.howansky@gmail.com>
2016-08-01 15:56:58 +0000

    Updated unpack() function.

--- .my.bash
+++ .my.bash
@@ -65,11 +65,18 @@ function gcp()
 
 function unpack()
 {
+    [ -z ${1} ] && { echo "Usage: ${0} <package>"; return 1; }
+    [ -f ${1} ] || { echo "No such package."; return 1; }
+    DIR=${1%.tar.*}
+    [ -d ${DIR} ] && { echo "Package already extracted."; return 1; }
     [ -f ${1}.asc ] && { gpg --verify ${1}.asc || return 1; }
     [ -f ${1}.sig ] && { gpg --verify ${1}.sig || return 1; }
-    tar xf ${1}
-    chmod -R go-rwxs ${1%.tar.*}
-    chown -R root:root ${1%.tar.*}
+    tar xf ${1} || return 1
+    [ -d ${DIR} ] || return 1
+    chmod -R go-rwxs ${DIR}
+    chown -R root:root ${DIR}
+    rm -f ${1} ${1}.asc ${1}.sig
+    cd ${DIR}
 }
 
 umask 077
