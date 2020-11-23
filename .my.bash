f124014845727def9c9bd31439dfd3eace10b0e3
Alex Howansky <alex.howansky@cisecurity.org>
2020-11-23 11:23:20 -0500

    Added gdb, gdr, glb, glr bash functions in place of git aliases.

--- .my.bash
+++ .my.bash
@@ -2,73 +2,75 @@ function _update_ps1() {
     PS1=$(~/bin/powerline-shell.py --cwd-mode plain --mode patched $?)
 }
 
-function ga()
-{
+function ga() {
     git add ${*}
 }
 
-function gb()
-{
+function gb() {
     git branch ${*}
 }
 
-function gc()
-{
+function gc() {
     git commit ${*}
 }
 
-function gco()
-{
+function gco() {
     git checkout ${*}
 }
 
-function gcp()
-{
+function gcp() {
     git cherry-pick -n -x ${*}
 }
 
-function gd()
-{
+function gd() {
     git diff ${*}
 }
 
-function gdc()
-{
+function gdc() {
     git diff --cached ${*}
 }
 
-function gf()
-{
+function gdb() {
+    gd ${1:-develop}...HEAD
+}
+
+function gdr() {
+    gd HEAD...${1:-develop}
+}
+
+function gf() {
     git find ${*}
 }
 
-function gl()
-{
+function gl() {
     git log --name-status ${*}
 }
 
-function gp()
-{
+function glb() {
+    gl ${1:-develop}...HEAD
+}
+
+function glr() {
+    gl HEAD..${1:-develop}
+}
+
+function gp() {
     git pull
 }
 
-function gs()
-{
+function gs() {
     git status
 }
 
-function gu()
-{
+function gu() {
     git reset --soft HEAD~1
 }
 
-function title()
-{
+function title() {
     echo -en "\e]2;$1\a"
 }
 
-function unpack()
-{
+function unpack() {
     [ -z ${1} ] && { echo "Usage: ${0} <package>"; return 1; }
     [ -f ${1} ] || { echo "No such package."; return 1; }
     DIR=${1%.tar.*}
